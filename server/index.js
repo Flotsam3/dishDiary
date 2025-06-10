1755695e66abdef025786b57c428fcfc4dae5d6d
flotsam <mpb1@e.mail.de>
2025-06-10 13:20:15 +0200

    feat: increase express file size limit

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import archiveRoutes from './routes/archiveRoutes.js';
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
